0d5307e98056a6b27f5ee01cc04c2bf408624b3d
Martin Štrekelj <martin.strekelj@easy.bi>
2022-01-15 10:54:21 +0100

    wallet: add basic front page

---
+++ apps/wallet/src/app/components/Button.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+import t from '../shared/theme';
+
+interface ButtonProps {
+  onPress?: () => void;
+  styles?: any[];
+  label?: string;
+}
+
+export const Button = ({
+  onPress = () => console.warn('pressed'),
+  styles,
+  label = 'Button',
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={StyleSheet.compose(
+        [
+          t.p4,
+          t.bgPrimary,
+          t.roundedSm,
+          t.flex,
+          t.flexRow,
+          t.justifyCenter,
+          t.itemsCenter,
+          t.m4,
+        ],
+        styles
+      )}
+      onPress={onPress}
+    >
+      <Text style={[t.textXl, t.uppercase, t.fontSansBold]}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
